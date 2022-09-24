dbcd4c193d0c580bae4267f39bc63ae41b3100c9
george-hub331 <idiaghegeorge9@gmail.com>
2022-09-24 14:18:46 +0100

    update: add cronos chain

--- app/contexts/Cryptea/connectors/chains.ts
+++ app/contexts/Cryptea/connectors/chains.ts
@@ -1,10 +1,73 @@
-import { configureChains, defaultChains, chain } from "wagmi";
+import { configureChains, defaultChains, chain, Chain } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { alchemyProvider } from "wagmi/providers/alchemy";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const avalancheChain: Chain = {
+  id: 43_114,
+  name: "Avalanche",
+  network: "avalanche",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Avalanche",
+    symbol: "AVAX",
+  },
+  rpcUrls: {
+    default: "https://api.avax.network/ext/bc/C/rpc",
+  },
+  blockExplorers: {
+    default: { name: "SnowTrace", url: "https://snowtrace.io" },
+  },
+  testnet: false,
+};
+
+const Cronos: Chain = {
+  id: 25,
+  name: "Cronos",
+  network: "cronos mainnet",
+  nativeCurrency: {
+    name: "cronos",
+    decimals: 18,
+    symbol: "CRO",
+  },
+  rpcUrls: {
+    default: "https://evm.cronos.org",
+  },
+  blockExplorers: {
+    default: { name: "cronos", url: "https://cronos.org/explorer" },
+  },
+  testnet: false,
+};
+
+const CronosTest: Chain = {
+  id: 338,
+  name: "Cronos Testnet",
+  network: "cronos",
+  nativeCurrency: {
+    name: "cronos",
+    decimals: 18,
+    symbol: "TCRO",
+  },
+  rpcUrls: {
+    default: "https://evm-t3.cronos.org",
+    wss: "wss://cronos-testnet-3.crypto.org:8546",
+  },
+  blockExplorers: {
+    default: {name: "cronos-test", url: ""},
+  },
+  testnet: true
+};
 
 export const { chains, provider, webSocketProvider } = configureChains(
-  [chain.polygon, chain.polygonMumbai, chain.mainnet],
+  [chain.polygon, chain.polygonMumbai, avalancheChain, CronosTest, Cronos],
   [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== avalancheChain.id || chain.id !== CronosTest.id || chain.id !== Cronos.id) return null
+
+        return { http: chain.rpcUrls.default }
+      }
+    }),
     publicProvider()
   ]
 );
