ba2827285d7e3cf514a276e2e059de8f76736a77
Temiloluwa Adelowo <moboluwaji003@gmail.com>
2023-01-07 22:50:22 +0100

    fix: Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,50 @@
-import '../styles/globals.css';
-import '../styles/Home.css';
-import '../styles/Dash.css';
-import '../styles/Auth.css';
-import '../styles/Sett.css';
+import "../styles/globals.css";
+import "../styles/Home.css";
+import "../styles/Dash.css";
+import "../styles/Auth.css";
+import "../styles/Sett.css";
 
-import type { AppProps } from 'next/app'
-import React from 'react'
-import { GenProvider } from '../app/contexts/GenContext';
-import { HomeProvider } from '../app/contexts/HomeContext';
-import '../app/contexts/Cryptea/types.d.ts';
+import type { AppProps } from "next/app";
+import React from "react";
+import { GenProvider } from "../app/contexts/GenContext";
+import { HomeProvider } from "../app/contexts/HomeContext";
+import "../app/contexts/Cryptea/types.d.ts";
 import { CrypteaProvider } from "../app/contexts/Cryptea/Auth";
-import { crypteaCon, stylesCon } from '../app/contexts/Cryptea/icon';
-import Script from 'next/script';
-import Head from 'next/head'
+import { crypteaCon, stylesCon } from "../app/contexts/Cryptea/icon";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const once = React.useRef<boolean>(true)
+  const once = React.useRef<boolean>(true);
 
   React.useEffect(() => {
-
     if (once.current) {
-
       once.current = false;
 
       console.log(`${crypteaCon} \n\n   %c Pay with Cryptea😊`, stylesCon);
-
     }
-
-  }, [])
+  }, []);
 
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-      <Script id="my-script" strategy="lazyOnload">
-        {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-      </Script>
-      <Script strategy="lazyOnload" src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+`,
+        }}
+      />
 
       <Head>
         <title>Welcome!</title>
